8349dcfcb086c8dfd65cc689d16edbf2032df655
Fernanda Palapa <fernandabqd@gmail.com>
2022-11-12 20:54:29 -0600

    add:table product

--- Ob_bears1/src/main/java/com/odcarebears/entity/Categoria.java
+++ Ob_bears1/src/main/java/com/odcarebears/entity/Categoria.java
@@ -9,7 +9,7 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	@Column(name="id_categoria")
-	private Long id_Categoria;
+	private Long idCategoria;
 	
 	@Column(name="nombre_categoria", length=100)
 	private String nombreCategoria;
--- Ob_bears1/src/main/java/com/odcarebears/entity/Producto.java
+++ Ob_bears1/src/main/java/com/odcarebears/entity/Producto.java
@@ -1,5 +1,47 @@
 package com.odcarebears.entity;
 
+import java.util.Calendar;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="producto")
 public class Producto {
+	
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name="id_producto")
+	private Long id_Producto;
+	
+	@Column(name="nombre_producto")
+	private String numeroProductos;
+	
+	@Column(name="precio" )
+	private Double precio;
+	
+	@Column(name="descripcion", length=255)
+	private String descripcion;
+	
+	@Column(name="fecha_creacion", insertable=false, updatable=false, columnDefinition="timestamp default CURRENT_TIMESTAMP")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar fechaCreacion;
+	
+	@Column(name="existencias")
+	private Long estado;
+	
+	//uno a muchos con (imagenes>id_producto)
+	//uno a muchos con (detalles pedido>id_producto)  
+	
+	/*
+	@ManyToMany
+	@JoinColumn(name="id_categoria")
+	private Categoria idCategoria
+	 */
+	;
+
 
 }
+
+
+
